8c93ddb682b6ed168a808e6465ad94b21a3750f9
Retji Josiah Dakon <retjidakon@yahoo.com>
2023-02-23 16:36:27 +0100

    Update NigeriaDecides.py

--- NigeriaDecides.py
+++ NigeriaDecides.py
@@ -60,7 +60,7 @@ Election_data = pd.DataFrame(Election_tweets,
 
 #removing the "+00:00" in the datetime
 Election_data["Date_tweeted"]=pd.to_datetime(Election_data['Date_tweeted']).dt.strftime('%Y-%m-%d %H:%M:%S')
-
+import os
 # Load the existing data into a DataFrame
 filename = "Election_data.csv"
 existing_data = pd.read_csv(filename)
